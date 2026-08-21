6477e613d39d9e54fb4a988fe80d34bb24b8d717
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2026-08-20 23:58:10 -0400

    Update fxamanifest.lua with additional metadata
    Added Lua version support and metadata for the project.

--- fxamanifest.lua
+++ fxamanifest.lua
@@ -1,2 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
+
+author 'Popcorn RP'
+description 'Optional ox_lib zone controller for mp2025_02 exterior mansion map components + Health/Armor/Stamina Removal for 3751'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
